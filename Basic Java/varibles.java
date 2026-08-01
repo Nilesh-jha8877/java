2529fa2a3dad7408883a25b63ea5cf012830abb7
nilesh kumar <nileshkumar76349@gmail.com>
2026-08-01 15:54:53 +0530

    Variables in java

---
+++ Basic Java/varibles.java
@@ -0,0 +1,15 @@
+package helloworld.java;
+
+public class varibles {
+    static void main() {
+        int x;
+        x=90;
+        System.out.println(x);
+        x=x-20;
+        System.out.println(x);
+        x=x*6;
+        System.out.println(x);
+        x=x/7;
+        System.out.println(x);
+    }
+}
