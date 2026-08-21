98fa7274c5e8661d930e12f5f0d09390a10ecf5f
Nilesh Kumar Jha <nileshkumar76349@gmail.com>
2026-08-21 16:21:49 +0530

    About operators of java

---
+++ Basic Java/incrementDecrement.java
@@ -0,0 +1,15 @@
+package helloworld.java;
+
+public class incrementDecrement {
+    static void main(String[] args) {
+        int x=10;
+       // System.out.println(x);
+//        x++;
+//        ++x;
+//       System.out.println(x);
+//       x++;
+//        System.out.println(x);
+        int y=x++;
+        System.out.println(x+" \n"+y);
+    }
+}
