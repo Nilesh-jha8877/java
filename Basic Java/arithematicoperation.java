fd530418eaafe82c0296b6baecccddeb74bfff5e
Nilesh Kumar Jha <nileshkumar76349@gmail.com>
2026-08-21 16:14:22 +0530

    About arthematic operation

---
+++ Basic Java/arithematicoperation.java
@@ -0,0 +1,19 @@
+package helloworld.java;
+
+public class arithematicoperation {
+    static void main() {
+//        int x=39 , y=7;
+//        System.out.println(x+y);
+//        System.out.println(x-y);
+//        System.out.println(x*y);
+//        System.out.println(x/y);
+//        System.out.println(x%y);
+        double w=38,z=98;
+        System.out.println(w+z);
+        System.out.println(w-z);
+        System.out.println(w*z);
+        System.out.println(w/z);
+        System.out.println(w%z);
+
+    }
+}
