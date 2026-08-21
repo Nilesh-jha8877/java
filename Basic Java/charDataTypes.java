52abf88182f2ab65d8693a05857f2d996061d514
Nilesh Kumar Jha <nileshkumar76349@gmail.com>
2026-08-21 16:18:57 +0530

    About character in java

---
+++ Basic Java/charDataTypes.java
@@ -0,0 +1,17 @@
+package helloworld.java;
+
+public class charDataTypes {
+    static void main(String[] args) {
+//        char ch='A';
+//        int x=ch;
+//        System.out.println(x);
+//        char ch1='9';
+//        int y=ch1;
+//        System.out.println(y);
+//        System.out.println(ch*ch);
+        int x=67;
+        char ch=(char)x;
+        System.out.println(ch);
+
+    }
+}
