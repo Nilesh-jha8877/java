187b70fe08049e84a25c8983db6b3143425a7b8a
Nilesh Kumar Jha <nileshkumar76349@gmail.com>
2026-09-26 12:43:07 +0530

    Use of loops

---
+++ LoopsInJava/Basicloops.java
@@ -0,0 +1,14 @@
+package LoopsInJava;
+
+import java.util.Scanner;
+
+public class Basicloops {
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number :");
+        int n=sc.nextInt();
+        for (int i=0;i<n;i++){
+            System.out.println("Nikita hates amir");
+        }
+    }
+}
